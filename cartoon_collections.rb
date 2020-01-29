98d1a421c02977e74b93224b1254030fb7e9f2d7
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-29 13:02:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def long_planeteer_calls(calls)
   return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
