df48f0c8990f17acdd04ba2d448626fab16bf45c
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-29 12:32:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-planeteer_calls.map! {|word| word + "!"}
+planeteer_calls.map! {|word| word.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
