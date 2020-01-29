199a12c2f8ac017e021f70908236f0989a17d865
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-29 13:13:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,8 +21,7 @@ end
 
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  puts snack
-   snacks.detect{|snack| snack.include?(cheese_types)}
+  snacks.detect{|snack|   puts snack } #snack.include?(cheese_types)}
 end
 
   # the array below is here to help
